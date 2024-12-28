2d984c1c6cb86cdab72ac57215a2db83239d8cf2
premlataaa <premat5567@gmail.com>
2024-12-28 15:06:47 +0530

    login Page done

---
+++ frontend/scripts/index.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("form")
+
+form.addEventListener("submit", function(){
+    event.preventDefault()
+
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log(email,password)
+
+    if(email=="admin@empher.com" && password== "empher@123"){
+        alert("redirecting to admin page")
+        window.location.href="admin.html"
+    }else if(email=="user@empher.com" && password== "user@123"){
+        alert("redirecting to Books")
+        window.location.href="books.html"
+    }else{
+        alert("Please submit correct Email or Password")
+    }
+})
